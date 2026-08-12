7dffe35a6d2d634094d9885870b3928b9f26f97b
Arman Maulana <azkaowi@gmail.com>
2026-08-12 15:41:12 +0700

    implement middleware

--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -1,14 +1,11 @@
 import express from "express";
-import {
-  getTicket,
-  useTicket,
-} from "../controllers/ticketController.js";
+import { getTicket, useTicket } from "../controllers/ticketController.js";
 import { isAdmin, protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 // router.get("/:ticket_id/verify", protect, isAdmin, scanTicket);
 router.get("/:ticket_id", getTicket);
-router.patch("/:ticket_id", useTicket);
+router.patch("/:ticket_id", protect, isAdmin, useTicket);
 
 export default router;
